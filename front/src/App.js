029600c459adc5f4a4bcc30c2d0d65768eb52986
FRosay <felix.rosay@laposte.net>
2020-05-30 21:04:17 +0200

    reorganized the router paths, added a link to go back to the login, deleted a file from the initial cloned projet that was forgotten

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import GamesList from './components/game/games-list';
-import GameCreation from './components/game/game-creation';
-import MembersList from './components/member/members-list';
-import MemberCreation from './components/member/member-creation';
-import LoansList from './components/loan/loans-list';
-import LoanCreation from './components/loan/loan-creation';
+
 
 import Login from './components/login/Login';
+import Home from './Home';
 
-import { Tabs, Tab } from 'react-bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
@@ -20,50 +15,21 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      isAuthenticated: false,
-    }
+    this.state = {}
   }  
 
   render () {
-    if (false === true) {
-      return  (
-        <div className="App">
-          <Tabs defaultActiveKey="members" id="main-tab">
-            <Tab eventKey="members" title="Adhérent.e.s">
-              <div className='defaultDiv'>
-                <MemberCreation /><MembersList />
-              </div>
-            </Tab>
-            <Tab eventKey="games" title="Jeux">
-              <div className='defaultDiv'>
-                <GameCreation /><GamesList />
-              </div>
-            </Tab>
-            <Tab eventKey="loans" title="Prêts">
-              <div className='defaultDiv'>
-                <LoanCreation /><LoansList />
-              </div>
-            </Tab>
-          </Tabs>
+    return (
+      <Router>
+        <div className= 'App'>
+          <Switch>
+            <Route exact path= '/' component= { Login } />
+            <Route path= '/sign-in' component= { Login } />
+            <Route path= '/home' component= { Home } />
+          </Switch>
         </div>
-      )
-    } else {
-      return (
-        <Router>
-          <div className="App">
-            <div className="auth-wrapper">
-              <div className="auth-inner">
-                <Switch>
-                  <Route exact path='/' component={Login} />
-                  <Route path="/sign-in" component={Login} />
-                </Switch>
-              </div>
-            </div>
-          </div>
-        </Router>
-      )
-    }
+      </Router>
+    )
   }
 }
 
--- front/src/components/login/Login.js
+++ front/src/components/login/Login.js
@@ -1,27 +1,43 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 
 class Login extends Component {
     render() {
         return (
-            <form>
-                <h3>Connexion</h3>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <form>
+                        <h3>Connexion</h3>
+                        
+                        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+                            <div className="container">
+                                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                                <ul className="navbar-nav ml-auto">
+                                    <li className="nav-item">
+                                    <Link className="nav-link" to={"/home"}>Voir le site</Link>
+                                    </li>
+                                </ul>
+                                </div>
+                            </div>
+                        </nav>
+                        <div className="form-group">
+                            <label>Nom d'utilisateur</label>
+                            <input type="email" className="form-control" placeholder="Entrez votre nom d'utilisateur" />
+                        </div>
 
-                <div className="form-group">
-                    <label>Utilisateur</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                        <div className="form-group">
+                            <label>Mot de passe</label>
+                            <input type="password" className="form-control" placeholder="Entrez votre mot de passe" />
+                        </div>
+                        <br />
+                        <button type="submit" className="btn btn-primary btn-block">Se connecter</button>
+                        <p className="forgot-password text-right">
+                            Mot de passe oublié ?
+                        </p>
+                    </form>
                 </div>
-
-                <div className="form-group">
-                    <label>Mot de passe</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-                <br />
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Mot de passe oublié ?
-                </p>
-            </form>
+            </div>
         );
     }
 }
